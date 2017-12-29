6694257b2ce3fad101c4a91f847f273dc8ad049f
psou <adh.workshops@gmail.com>
2017-12-29 14:49:38 +0000

    initial commit

---
+++ backend/src/main/java/com/arhs/hellas/testarhs/TestarhsApplication.java
@@ -0,0 +1,12 @@
+package com.arhs.hellas.testarhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestarhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestarhsApplication.class, args);
+	}
+}
